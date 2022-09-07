3dd947bb3dc3e39dd84613dfc4b629007daaf2e6
Andrea Nuñez <106264362+AndreNuez@users.noreply.github.com>
2022-09-07 00:19:45 -0300

    load-closing events

---
+++ TPWinForm_Bossetti_Nunez/View/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace View
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bienvenido usuario");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show("Gracias por usar nuestra aplicación");
+        }
+    }
+}
