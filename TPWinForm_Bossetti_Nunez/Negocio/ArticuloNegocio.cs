07e1781f10b9391add9966b584f340132d91389a
GBossetti <gonzalo.bossetti@gmail.com>
2022-09-07 14:49:15 -0300

    Se agrega Clase AccesoDatos

---
+++ TPWinForm_Bossetti_Nunez/Negocio/ArticuloNegocio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Modelo;
+
+namespace Negocio
+{
+    public class ArticuloNegocio
+    {
+        public List<Articulo> listar()
+        {
+            List<Articulo> lista = new List<Articulo>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("--Consulta SQL");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+
+                }
+
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
