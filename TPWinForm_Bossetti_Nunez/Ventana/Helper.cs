72ea3da38722cfedb3fca3b2f8606d9a39dd9ef7
GBossetti <gonzalo.bossetti@gmail.com>
2022-09-18 22:40:29 -0300

    Se agrega validación a Campo Precio

--- TPWinForm_Bossetti_Nunez/Ventana/Helper.cs
+++ TPWinForm_Bossetti_Nunez/Ventana/Helper.cs
@@ -10,15 +10,20 @@ namespace Ventana
     {       
         public bool validarNumeros(string cadena)
         {
-            foreach (char caracter in cadena)
+            if(cadena.Count(x=> x == '.') > 1)
             {
+                return false;
+            }
+
+            foreach (char caracter in cadena)
+            {   
                 if (!(char.IsNumber(caracter) || char.IsPunctuation(caracter)))
                 {
                     return false;
                 }
             }
 
-            if (cadena.Contains("-"))
+            if (cadena.Contains("-") || cadena.Contains(","))
             {
                 return false;
             }
